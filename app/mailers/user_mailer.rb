8c5b35f833e8c27a8802b09dfff31728b01c5da2
Speaslee <sophiapeaslee@gmail.com>
2015-11-06 14:43:39 -0500

    mailer stuff

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,13 +3,13 @@ class UserMailer <ApplicationMailer
     @repair = repair
     User.where(admin:true).each do |manager|
 
-      mail(to: manager.email, subject: "#{@repair.user.aptnum} repair request: #{@repair.name}" )
+      mail(to: manager.email, subject: "#{@repair.user.aptnum} repair request: #{@repair.name}" , body: "#{@repair.description}")
     end
   end
 
   def complete_repair repair
     @repair = repair
-    mail(to: @repair.user.email, subject: "Your repair request has been completed")
+    mail(to: @repair.user.email, subject: "Your repair request has been completed", body: "Your repair request for #{@repair.name} has been completed")
   end
 
   def send_alert user
